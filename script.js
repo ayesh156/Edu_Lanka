126edc43b5607f69bb4b253726afbb15029e706d
ayesh156 <ayeshchathuranga531@gmail.com>
2022-02-13 11:35:24 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+/* ------------ Toggle Menu ------------ */
+
+var navLinks = document.getElementById("navLinks");
+function showMenu(){
+    navLinks.style.right = "0";
+}
+function hideMenu(){
+    navLinks.style.right = "-200px";
+}
+
+/* ------------ Testimonial Swiper ------------ */
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        700: {
+            slidesPerView: 3,
+        },
+        
+    },
+});
